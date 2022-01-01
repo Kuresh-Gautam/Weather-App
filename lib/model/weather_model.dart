600e7788bd60df4421ee1e175622a74cc41c614f
Kuresh Gautam <gautamkulashekhar@gmail.com>
2022-01-01 09:14:17 +0545

    created weather model

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,17 @@
+class Weather {
+  String? cityName;
+  String? temp;
+  double? wind;
+  int? humidity;
+  double? feelsLlike;
+  int? pressure;
+
+  Weather({
+    required this.cityName,
+    required this.temp,
+    required this.wind,
+    required this.humidity,
+    required this.feelsLlike,
+    required this.pressure,
+  });
+}
